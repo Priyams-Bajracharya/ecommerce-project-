db039a2637ba4966a3b11f7f75456d3c4edccbee
Priyams-Bajracharya <priyams.bajracharya@gmail.com>
2025-09-24 21:08:12 +0545

    get cart from backend and display quantity

--- ecomerce-project/src/Pages/HomePage.jsx
+++ ecomerce-project/src/Pages/HomePage.jsx
@@ -7,18 +7,25 @@ import './HomePage.css';
 export function HomePage() {
 
   const [products,setProducts] =useState([]);
+  cont [cart,setCart]=useState([]);
+
   useEffect(() => {
     axios.get('http://localhost:3000/api/products')
       .then((response) => {
         setProducts(response.data);
       });
+
+    axios.get('http://localhost:3000/api/cart-items')
+      .then((response)=>{ 
+        setCart(response.data);
+      })
   },[]);
 
 
 
   return (
     <>
-      <Header />
+      <Header cart={cart}/>
 
       <title>Home Page</title>
 
