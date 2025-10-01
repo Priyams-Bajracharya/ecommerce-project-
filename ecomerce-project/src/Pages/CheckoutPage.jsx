7f40db94efa234214f2dd46820934e478a72b392
Priyams-Bajracharya <priyams.bajracharya@gmail.com>
2025-10-01 12:20:28 +0545

    Revert "generate html for delivery options"
    This reverts commit 8f8df718196fecc76335fc7393c89d4579a47e67.

--- ecomerce-project/src/Pages/CheckoutPage.jsx
+++ ecomerce-project/src/Pages/CheckoutPage.jsx
@@ -7,19 +7,12 @@ import { formatMoney } from '../utils/money';
 
 export function CheckoutPage({ cart }) {
   const [deliveryOptions, setDeliveryOptions] = useState([]);
-  const [paymentSummary,setPaymentSummary] = useState(null);
 
   useEffect(() => {
     axios.get('/api/delivery-options?expand=estimatedDeliveryTime')
       .then((response) => {
         setDeliveryOptions(response.data);
-      });
-
-    axios.get('/api/payment-summary')
-    .then((response)=>{
-      setPaymentSummary(response.data);
-
-    })
+      })
   }, [])
   return (<>
 
